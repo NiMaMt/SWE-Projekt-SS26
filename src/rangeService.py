c7069be6df114161aa405e9b191c35351512ed96
Jan Fiedler <jafimt00@hs-esslingen.de>
2026-06-24 22:29:09 +0200

    Methode calculateEnergyAvailable, ersten Test erstellt

--- src/rangeService.py
+++ src/rangeService.py
@@ -1,4 +1,7 @@
 class rangeService:
+    def calculateEnergyAvailable(vehicle, percentageEnergyLeft):
+        return 0
+
     def checkDrivePossible(energyAvailable, energyReqired):
         if(energyAvailable >= energyReqired):
             return True
--- tests/test_rangeService.py
+++ tests/test_rangeService.py
@@ -1,4 +1,5 @@
 from src.rangeService import rangeService
+from src.vehicle_profile import VehicleProfile
 
 def test_rangeService_checkDrivePossible():
     # energyAvailable < energyReqired
@@ -12,3 +13,9 @@ def test_rangeService_checkDrivePossible():
     # energyAvailable = energyReqired
     is_possible_test = rangeService.checkDrivePossible(energyAvailable=50, energyReqired=50)
     assert(is_possible_test == True)
+
+def test_rangeService_calculateEnergyAvailable():
+    testVehicle = VehicleProfile(name = 'Testvehicle', capacity_kwh = 85, average_consumption_wh_km = 160, range_km = 531.25, weight_kg = 2100)
+
+    energyAvailable = rangeService.calculateEnergyAvailable(testVehicle, 0.6)
+    assert(energyAvailable == 100)
