b31abdb3f114c247a8bc933fc759fdb22d2c7ff2
Jan Fiedler <jafimt00@hs-esslingen.de>
2026-06-24 22:43:19 +0200

    #22 - Berechnung noch verfügbarer Energie im Akku

--- src/rangeService.py
+++ src/rangeService.py
@@ -1,6 +1,8 @@
+from src.vehicle_profile import VehicleProfile
+
 class rangeService:
     def calculateEnergyAvailable(vehicle, percentageEnergyLeft):
-        return 0
+        return vehicle.capacity_kwh * percentageEnergyLeft
 
     def checkDrivePossible(energyAvailable, energyReqired):
         if(energyAvailable >= energyReqired):
