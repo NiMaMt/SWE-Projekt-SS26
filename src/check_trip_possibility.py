ee0723a25155c104d08b6b3616bed507e073ae25
Sven Wurster <svwumt00@hs-esslingen.de>
2026-06-24 16:09:06 +0200

    Merge feature branch - add check_trip_possibility.py

---
+++ src/check_trip_possibility.py
@@ -0,0 +1,6 @@
+class CheckTripPossibility:
+    def __init__(self, trip_possible, lack_capacity_kwh, residual_capacity_kwh):
+        self.trip_possible = trip_possible
+        self.lack_capacity_kwh = lack_capacity_kwh
+        self.residual_capacity_kwh = residual_capacity_kwh
+        
