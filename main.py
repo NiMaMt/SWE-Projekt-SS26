0833c8fd41bf3a0c1a370e2ea643e7b736acf6cf
Sven Wurster <svwumt00@hs-esslingen.de>
2026-06-26 15:46:12 +0200

    Rechtschreibkorrektur, Ausgaben Anpassung Main Code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.rangeService import RangeService
 #Hilfsmethoden
 #-------------------------------------------------------------------------------
 
-def controll_user_input_integer(prompt, min_value=None, max_value=None):
+def control_user_input_integer(prompt, min_value=None, max_value=None):
     
     # Kontrolliert die Benutzereingabe von Integer Werten mit Wiederholung der Eingabeaufforderung bei ungültiger Eingabe.
 
@@ -25,7 +25,7 @@ def controll_user_input_integer(prompt, min_value=None, max_value=None):
         except ValueError:
             print("Ungültige Eingabe! Bitte geben Sie eine ganze Zahl ein.")
 
-def controll_user_input_float(prompt, min_value=None, max_value=None):
+def control_user_input_float(prompt, min_value=None, max_value=None):
     
     # Kontrolliert die Benutzereingabe von Float Werten mit Wiederholung der Eingabeaufforderung bei ungültiger Eingabe.
 
@@ -108,7 +108,7 @@ def main():
         ConsoleUI.print_vehicle(vehicles)
         print()
 
-        selected_vehicle_number = controll_user_input_integer("Wählen Sie ein Fahrzeug durch Eingabe der entsprechenden Nummer", 1, len(vehicles))
+        selected_vehicle_number = control_user_input_integer("Wählen Sie ein Fahrzeug durch Eingabe der entsprechenden Nummer", 1, len(vehicles))
         trip_config.select_vehicle(vehicles, selected_vehicle_number)
         print(f"Gewähltes Fahrzeug: {trip_config.vehicle.name}\n")
         
@@ -117,7 +117,7 @@ def main():
         ConsoleUI.print_weather(weathers)
         print()
         
-        selected_weather_number = controll_user_input_integer("Wählen Sie ein Wetter durch Eingabe der entsprechenden Nummer", 1, len(weathers))
+        selected_weather_number = control_user_input_integer("Wählen Sie ein Wetter durch Eingabe der entsprechenden Nummer", 1, len(weathers))
         trip_config.select_weather(weathers, selected_weather_number)
         print(f"Gewähltes Wetter: {trip_config.weather.name}\n")
         
@@ -126,13 +126,13 @@ def main():
         ConsoleUI.print_route(routes)
         print()
         
-        selected_route_number = controll_user_input_integer("Wählen Sie eine Route durch Eingabe der entsprechenden Nummer", 1, len(routes))
+        selected_route_number = control_user_input_integer("Wählen Sie eine Route durch Eingabe der entsprechenden Nummer", 1, len(routes))
         trip_config.select_route(routes, selected_route_number)
         print(f"Gewählte Route: {trip_config.route.name}\n")
         
         #Batteriestand eingeben
         print("=== Batteriestand ===")
-        capacity_percent = controll_user_input_float("Geben Sie den aktuellen Batteriestand in Prozent ein", 0,100)
+        capacity_percent = control_user_input_float("Geben Sie den aktuellen Batteriestand in Prozent ein", 0,100)
         trip_config.set_capacity_percent(capacity_percent)
         print(f"Eingegebener Batteriestand: {capacity_percent}%\n")
         
@@ -153,40 +153,15 @@ def main():
         else:
             print("!ACHTUNG! Das Erreichen des Ziels ist mit Ihrem aktuellen Batteriestand NICHT möglich")
             print(f"Fehlende Kapazität: {trip_possibility.lack_capacity_kwh:.2f} kWh")
-            chargeOption = get_valid_string_choice("Möchten Sie eine Ladestation auf Ihrer Route suchen? (J/N)", "J", "N")
+            user_decision = get_valid_string_choice("Möchten Sie eine Ladestation auf Ihrer Route suchen? (J/N)", "J", "N")
             print(f"Diese Funktion ist mit ihrem aktuellen Abonnement leider nicht verfügbar. Bitte kontaktieren Sie ihren Service Partner für weitere Informationen.\n")
-
-
-
 
         return True
 
     except Exception as e:
         print(f"Unerwarteter Fehler: {e}")
         return False
-
-
-# Späterer Ablauf:
-    # vehicles kommt von DataLoader
-    # selected_vehicle_number = int(input("Wählen Sie ein Fahrzeug: "))
-    # TripConfiguration.select_vehicle(vehicles, selected_vehicle_number)
-    #
-    # selected_weather_number = int(input("Wählen Sie eine Wettersituation: "))
-    # TripConfiguration.select_weather(weathers, selected_weather_number)
-    #
-    # selected_route_number = int(input("Wählen Sie eine Route: "))
-    # TripConfiguration.select_route(routes, selected_route_number)
-    #
-    # capacity_percent = float(input("Geben Sie den aktuellen Batteriestand in Prozent ein: "))
-    # TripConfiguration.set_capacity_percent(capacity_percent)
- 
-    # 1. Objekte erzeugen mit DataLoader, quasi vehicles = load_vehicle_profiles()
-    # 2. Objekte übergeben an ConsoleUI an die print_vehicles(vehicles)
-    # 3. Objekt wählen mit TripConfiguration:
-    #       selected_vehicle_number = int(input("Wählen Sie ein Fahrzeug: "))
-    #       TripConfiguration.select_vehicle(vehicles, selected_vehicle_number)
-    # 4. Objekt vom Typ TripConfiguration erstellen mit den Werten aus den Eingaben. Da gibts bestimmt ne Anleitung wie das geht
-    # 5. RangeService Funktionen nutzen mit übergabe des TripConfiguration Objektes, rangeService.calculate_energy_available(TripConfiguration-Objekt)
+    
 
     
 if __name__ == "__main__":
